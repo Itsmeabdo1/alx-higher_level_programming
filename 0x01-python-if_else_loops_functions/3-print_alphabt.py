7ad782b713151b5ae3faf9c48cce568223ff0b9d
itsmeabdo1 <abderrahmane.meziouni@gmail.com>
2024-09-21 15:39:14 -0700

    0x01 python if else loops functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) == 'q' or chr(i) == 'e':
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) == 'e' or chr(i) == 'q':
         continue
-    print(chr(i).format(i), end='')
+    else:
+        print("{:s}".format(chr(i)), end="")
